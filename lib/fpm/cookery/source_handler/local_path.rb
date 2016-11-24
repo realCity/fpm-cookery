4b6b6ba0dfed78d372ed30ded0674c6da12ef54c
David Varga <david.varga@webvalto.hu>
2016-11-24 15:57:41 +0100

    If local path is a Git repo, clone it instead of copying.

--- lib/fpm/cookery/source_handler/local_path.rb
+++ lib/fpm/cookery/source_handler/local_path.rb
@@ -9,13 +9,48 @@ module FPM
         CHECKSUM = false
         NAME = :local_path
 
+        def fetch_repo(source, dest)
+          git_patch = cachedir/'working.patch'
+
+          safesystem "rm -rf #{dest}"
+          submodules = nil
+          Dir.chdir(source.to_s) do
+            submodules = `grep path .gitmodules | sed 's/.*= //'` if File.file?('.gitmodules')
+            Log.info "Running: git clone --shared #{source} #{dest}"
+            safesystem "git clone --shared #{source} #{dest}"
+            safesystem "git add -A"
+            safesystem "git diff --cached --binary > #{git_patch}"
+          end
+          Log.info "Patching source working directory changes"
+          # Only patch if the patch if non-empty
+          unless File.zero?("#{git_patch}")
+            Dir.chdir("#{dest}") do
+              safesystem "git apply --whitespace=nowarn #{git_patch}"
+              safesystem "rm #{git_patch}"
+            end
+          end
+
+          unless submodules == nil
+            submodules.each_line do |submodule|
+              Log.info "Copying submodule #{submodule}"
+              submodule = submodule.chomp
+              fetch_repo(source/submodule, dest/submodule)
+            end
+          end
+        end
+
         def fetch(config = {})
-          if local_path.exist?
+          if File.directory?(source.path+"/.git")
+            dest = cachedir/Path.new(source.path).basename
+            Log.info "Source is Git repo, using clone method."
+            fetch_repo(Path.new(source.path), dest)
+          elsif local_path.exist?
             Log.info "Using cached file #{local_path}"
           else
             Log.info "Copying #{source.path} to cache"
             FileUtils.cp_r(source.path, cachedir)
           end
+          Log.info "local_path:#{local_path}"
           local_path
         end
       end
